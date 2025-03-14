a49d8c9953017181367c644bfccb4864c1d2db84
Cauã Henrique <144502865+CauaHm@users.noreply.github.com>
2025-03-14 15:40:18 -0300

    adicionando interatividade ao favoritar mudando a cor do coração

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,14 @@
+  //Menu Hamburguer
 $(document).ready(function() {
+  
     $('.btn_mobile').on('click', function() {
        $('.mobile_nav_list_container').toggleClass('active');
-       $('.btn_mobile').find('i ').toggleClass('fa-x');
+       $('.btn_mobile').find('i').toggleClass('fa-x');
+    });  
+});
+
+$(document).ready(function() {
+    $('.dish-heart').on('click', function() {
+        $('.dish-heart').find('i').toggleClass('dish-heart-active');
     });
 });
